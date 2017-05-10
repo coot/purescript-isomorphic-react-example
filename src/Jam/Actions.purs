72b7bc6f26d797b94749acd3b90f8c9b7923e2c6
Marcin Szamotulski <mszamot@gmail.com>
2017-05-10 23:59:26 +0200

    MusCmd: EncodeJson and DecodeJson instances

--- src/Jam/Actions.purs
+++ src/Jam/Actions.purs
@@ -1,7 +1,10 @@
 module Jam.Actions where
 
 import Prelude
+import Control.Monad.Error.Class (throwError)
 import Control.Monad.Free (Free, liftF)
+import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, jsonEmptyObject, (:=), (~>), (.?))
+import Data.List (fromFoldable, toUnfoldable)
 import Jam.Types (Musician, NewMusician)
 
 data MusCmd a
@@ -9,6 +12,40 @@ data MusCmd a
   | RemoveMusician Int a
 
 derive instance functorCommand :: Functor MusCmd
+
+instance encodeJsonMusCmd :: EncodeJson (MusCmd a) where
+  encodeJson (AddMusician nm _)
+     = "type" := "AddMusician"
+    ~> "newMusician" := (
+           "name" := nm.name
+        ~> "description" := nm.description
+        ~> "wiki" := nm.wiki
+        ~> "generes" := (toUnfoldable nm.generes :: Array String)
+        ~> jsonEmptyObject
+    )
+    ~> jsonEmptyObject
+  encodeJson (RemoveMusician _id _)
+     = "type" := "RemoveMusician"
+    ~> "id" := _id
+    ~> jsonEmptyObject
+
+instance decodeJsonMusCmd :: DecodeJson (MusCmd (Array Musician -> Array Musician)) where
+  decodeJson json = do
+    obj <- decodeJson json
+    _type <- obj .? "type"
+    case _type of
+      "AddMusician" -> do
+        nm <- obj .? "newMusician"
+        name <- nm .? "name"
+        description <- nm .? "description"
+        wiki <- nm .? "wiki"
+        generesA <- nm .? "generes"
+        let generes = fromFoldable (generesA :: Array String)
+        pure $ AddMusician {name, description, wiki, generes} id
+      "RemoveMusician" -> do
+        _id <- obj .? "id"
+        pure $ RemoveMusician _id id
+      _ -> throwError "no such type"
 
 type MusDSL a = Free MusCmd a
 
